dac439b80305577dcfdfbf0cb4196b5a3768aed7
SpaCr <36156211+EinarOlafsson@users.noreply.github.com>
2023-12-08 14:24:49 -0500

    Update spacr_modify_masks_gui.py

--- spacr_modify_masks_gui.py
+++ spacr_modify_masks_gui.py
@@ -11,6 +11,7 @@ import imageio.v2 as imageio
 from skimage import morphology
 from collections import deque
 from skimage.exposure import rescale_intensity
+import matplotlib.pyplot as plt
 from scipy.ndimage import binary_dilation
 from skimage.morphology import disk
 import matplotlib as mpl
