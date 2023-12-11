5bbe845b388ac40ffa81c77760f0628d23830397
SpaCr <36156211+EinarOlafsson@users.noreply.github.com>
2023-12-11 10:49:30 -0500

    Update spacr_data_generation.py

--- spacr_data_generation.py
+++ spacr_data_generation.py
@@ -1892,6 +1892,7 @@ def generate_names(file_name, cell_id, cell_nuclei_ids, cell_parasite_ids, sourc
     return img_name, fldr, table_name
 
 def measure_crop_core(index, time_ls, file, settings):
+    global settings
     start = time.time()
     try:
         #files_to_process = len(os.listdir(settings['input_folder']))
