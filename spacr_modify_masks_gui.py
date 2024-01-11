4845faab151afe265894bb9cca1316013e2b1951
SpaCr <36156211+EinarOlafsson@users.noreply.github.com>
2024-01-10 20:37:56 -0500

    Update spacr_modify_masks_gui.py

--- spacr_modify_masks_gui.py
+++ spacr_modify_masks_gui.py
@@ -1,27 +1,71 @@
 import subprocess
 import sys
+import os
+import json
+import shutil
 
-# Check if the Python version is not 3.9
-if sys.version_info.major != 3 or sys.version_info.minor != 9:
-    current_version = f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
-    print(f"Workflow validated for Python 3.9, current version is {current_version}")
+def create_environment(env_name):
+    print(f"Creating environment {env_name}...")
+    subprocess.run(["conda", "create", "-n", env_name, "python=3.9", "-y"])
+    
+def install_dependencies_in_kernel(dependencies, env_name):
+    """Install dependencies in a specified kernel environment."""
+    
+    # Check if conda is available
+    conda_PATH = shutil.which("conda")
+    if not conda_PATH:
+        raise EnvironmentError("Conda executable not found.")
+    print("conda executable", conda_PATH)
+    
+    pip_PATH = f"{os.environ['HOME']}/anaconda3/envs/{env_name}/bin/python"
+    
+    # Update conda
+    print("Updating Conda...")
+    subprocess.run([conda_PATH, "update", "-n", "base", "-c", "defaults", "conda", "-y"])
 
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+    # Add conda-forge to channels
+    subprocess.run([conda_PATH, "config", "--add", "channels", "conda-forge"])
+    print("Added conda-forge to channels.")
 
-dependencies = [
-    "matplotlib", "numpy", "opencv-python", "scikit-image", 
-    "scipy", "Pillow", "imageio", "matplotlib", "PyQt5"
-]
+    # Install remaining dependencies with conda
+    for package in dependencies:
+        print(f"Installing {package}")
+        subprocess.run([conda_PATH, "install", "-n", env_name, package, "-y"])
 
-for package in dependencies:
-    try:
-        __import__(package)
-    except ImportError:
-        print(f"Installing {package}...")
-        install(package)
+    pip_packages = ["opencv-python"]
+    
+    for package in pip_packages:
+    	print(f"Installing {package}")
+    	subprocess.run([pip_PATH, "-m", "pip", "install", package])
 
-print('Dependencies installed')
+    print("Dependencies installation complete.")
+
+def add_kernel(env_name, display_name):
+    python_path = f"{os.environ['HOME']}/anaconda3/envs/{env_name}/bin/python"
+    kernel_spec = {
+        "argv": [python_path, "-m", "ipykernel_launcher", "-f", "{connection_file}"],
+        "display_name": display_name,
+        "language": "python"
+    }
+    kernel_spec_path = f"{os.environ['HOME']}/.local/share/jupyter/kernels/{env_name}"
+    os.makedirs(kernel_spec_path, exist_ok=True)
+    with open(os.path.join(kernel_spec_path, "kernel.json"), "w") as f:
+        json.dump(kernel_spec, f)
+
+env_name = "spacr_modify_masks_gui"
+
+dependencies = ["matplotlib", "seaborn", "scikit-image", "scipy", "Pillow", "imageio", "matplotlib", "PyQt5", "numpy"]
+
+env_PATH = f"{os.environ['HOME']}/anaconda3/envs/{env_name}"
+
+if not os.path.exists(env_PATH):
+	create_environment(env_name)
+	install_dependencies_in_kernel(dependencies, env_name)
+	add_kernel(env_name, env_name)
+	print(f"Environment '{env_name}' created and added as a Jupyter kernel.")
+	print(f"Refresh the page, set {env_name} as the kernel and run cell again")
+
+################################################################################################################################################################################
 
 import os, time, cv2, warnings, matplotlib
 matplotlib.use('Qt5Agg')
