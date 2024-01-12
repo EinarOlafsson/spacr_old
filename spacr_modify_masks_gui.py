18c4db2dc3e7a649cb28fe00292b11865b1f75d7
SpaCr <36156211+EinarOlafsson@users.noreply.github.com>
2024-01-12 12:23:16 -0500

    Update spacr_modify_masks_gui.py

--- spacr_modify_masks_gui.py
+++ spacr_modify_masks_gui.py
@@ -27,7 +27,7 @@ def install_dependencies_in_kernel(dependencies, env_name):
     	print(f"Installing {package}")
     	subprocess.run([conda_PATH, "install", "-n", env_name, package, "-y"])
     	
-    subprocess.run([pip_PATH, "-m", "pip", "install", "opencv-python"])
+    #subprocess.run([pip_PATH, "-m", "pip", "install", "opencv-python"])
     subprocess.run([pip_PATH, "-m", "pip", "install", "opencv-python-headless"]) #==4.9.0.80
     subprocess.run([pip_PATH, "-m", "pip", "install", "PyQt5"])
     print("Dependencies installation complete.")
