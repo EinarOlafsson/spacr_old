17f252b973e0c1b506369e7e50449e039368461d
SpaCr <36156211+EinarOlafsson@users.noreply.github.com>
2024-01-12 14:57:09 -0500

    Update spacr_modify_masks_gui.py

--- spacr_modify_masks_gui.py
+++ spacr_modify_masks_gui.py
@@ -3,12 +3,13 @@ import sys
 import os
 import json
 import shutil
+import platform
 
 def create_environment(env_name):
     print(f"Creating environment {env_name}...")
     subprocess.run(["conda", "create", "-n", env_name, "python=3.9", "-y"])
 
-#Install dependencies in a specified kernel environment.
+# Install dependencies in a specified kernel environment.
 def install_dependencies_in_kernel(dependencies, env_name):
     
     # Ensure Python version is 3.9 or above
@@ -21,22 +22,26 @@ def install_dependencies_in_kernel(dependencies, env_name):
         raise EnvironmentError("Conda executable not found.")
     print("conda executable", conda_PATH)
 
-    #define path to pip
-    pip_PATH = f"{os.environ['HOME']}/anaconda3/envs/{env_name}/bin/python"
+    # Determine the operating system
+    if platform.system() == "Windows":
+        # Windows path for pip executable
+        pip_PATH = f"{os.environ['USERPROFILE']}\\Anaconda3\\envs\\{env_name}\\python.exe"
+    else:
+        # Unix/Linux/MacOS path for pip executable
+        pip_PATH = f"{os.environ['HOME']}/anaconda3/envs/{env_name}/bin/python"
 
     # Update conda
     print("Updating Conda...")
     subprocess.run([conda_PATH, "update", "-n", "base", "-c", "defaults", "conda", "-y"])
     
     for package in dependencies:
-    	print(f"Installing {package}")
-    	subprocess.run([conda_PATH, "install", "-n", env_name, package, "-y"])
-    	#conda install fastai::opencv-python-headless
+        print(f"Installing {package}")
+        subprocess.run([conda_PATH, "install", "-n", env_name, package, "-y"])
     
-    #subprocess.run([pip_PATH, "-m", "pip", "install", "opencv-python"])
+    # Install additional packages
     subprocess.run([conda_PATH, "install", "-n", env_name, "opencv-python-headless", "-y"])
     subprocess.run([pip_PATH, "-m", "pip", "install", "PyQt5"])
-    	
+    
     print("Dependencies installation complete.")
     
 def add_kernel(env_name, display_name):
@@ -46,7 +51,12 @@ def add_kernel(env_name, display_name):
         "display_name": display_name,
         "language": "python"
     }
-    kernel_spec_path = f"{os.environ['HOME']}/.local/share/jupyter/kernels/{env_name}"
+    if platform.system() == "Windows":
+        env_PATH = f"{os.environ['USERPROFILE']}\\.local\\share\\jupyter\\kernels\\{env_name}"
+    else:
+        kernel_spec_path = f"{os.environ['HOME']}/.local/share/jupyter/kernels/{env_name}"
+    
+    
     os.makedirs(kernel_spec_path, exist_ok=True)
     with open(os.path.join(kernel_spec_path, "kernel.json"), "w") as f:
         json.dump(kernel_spec, f)
@@ -54,8 +64,6 @@ def add_kernel(env_name, display_name):
 env_name = "spacr_modify_masks_gui"
 
 dependencies = ["matplotlib==3.7.1", "scipy", "pillow", "scikit-image", "ipykernel", "requests", "h5py", "pyzmq"]
-
-env_PATH = f"{os.environ['HOME']}/anaconda3/envs/{env_name}"
 
 if not os.path.exists(env_PATH):
 	create_environment(env_name)
