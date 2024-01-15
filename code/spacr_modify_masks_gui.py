d9a39c5b62da22fe21cd7239252eb3cdfacaf7ce
SpaCr <36156211+EinarOlafsson@users.noreply.github.com>
2024-01-14 21:33:57 -0500

    Update spacr_modify_masks_gui.py

--- code/spacr_modify_masks_gui.py
+++ code/spacr_modify_masks_gui.py
@@ -629,7 +629,7 @@ def draw_thick_line(mask, y0, x0, y1, x1, thickness, mask_value):
     mask_line[rr, cc] = new_value
 
     # Dilate only the line
-    selem = square(thickness)  # Create a disk-shaped structuring element with the given radius
+    selem = square(int(thickness))  # Create a disk-shaped structuring element with the given radius
     dilated_line = dilation(mask_line, selem)
 
     # Apply the dilation to the original mask
