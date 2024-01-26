32201e21c149f9e24dc78f7df3330fc255ae0874
SpaCr <36156211+EinarOlafsson@users.noreply.github.com>
2024-01-26 14:52:07 -0500

    Update tkinter_masks_tool.py

--- code/tkinter_masks_tool.py
+++ code/tkinter_masks_tool.py
@@ -1,3 +1,118 @@
+import subprocess
+import sys
+import os
+import json
+import shutil
+import platform
+import getpass
+
+def get_paths(env_name):
+    conda_executable = "conda.exe" if sys.platform == "win32" else "conda"
+    python_executable = "python.exe" if sys.platform == "win32" else "python"
+    pip_executable = "pip.exe" if sys.platform == "win32" else "pip"
+
+    conda_path = shutil.which(conda_executable)
+    
+    if not conda_path:
+        if sys.platform == "win32":
+            conda_path = "C:\\ProgramData\\Anaconda3\\Scripts\\conda.exe"
+        else:
+            home_directory = os.path.expanduser('~')
+            conda_path = os.path.join(home_directory, 'anaconda3', 'bin', conda_executable)
+
+    if not os.path.exists(conda_path):
+        if sys.platform == "win32":
+            username = getpass.getuser()
+            conda_path = f"C:\\Users\\{username}\\Anaconda3\\Scripts\\conda.exe"
+
+    if not conda_path or not os.path.exists(conda_path):
+        print("Conda is not found in the system PATH")
+        return None, None, None, None
+
+    conda_dir = os.path.dirname(os.path.dirname(conda_path))
+    env_path = os.path.join(conda_dir, 'envs', env_name)
+    
+    if sys.platform == "win32":
+        pip_path = os.path.join(env_path, 'Scripts', pip_executable)
+        python_path = os.path.join(env_path, python_executable)
+    else:
+        python_path = os.path.join(env_path, 'bin', python_executable)
+        pip_path = os.path.join(env_path, 'bin', pip_executable)
+
+    return conda_path, python_path, pip_path, env_path
+
+# create new kernel
+def add_kernel(env_name, display_name):
+    _, python_path, _, _ = get_paths(env_name)
+    if not python_path:
+        print(f"Failed to locate the Python executable for '{env_name}'")
+        return
+
+    try:
+        subprocess.run([python_path, '-m', 'ipykernel', 'install', '--user', '--name', env_name, '--display-name', display_name])
+        print(f"Kernel for '{env_name}' with display name '{display_name}' added successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to add kernel. Error: {e}")
+        print(f"kernel can be added manualy with: python -m ipykernel install --user --name {env_name} --display-name {display_name}")
+
+def create_environment(conda_PATH, env_name):
+    print(f"Creating environment {env_name}...")
+    subprocess.run([conda_PATH, "create", "-n", env_name, "python=3.9", "-y"])
+
+# Install dependencies in a specified kernel environment.
+def install_dependencies_in_kernel(dependencies, env_name):
+    
+    conda_PATH, _, pip_PATH, _ = get_paths(env_name)
+
+    # Check if conda is available
+    if not conda_PATH:
+        raise EnvironmentError("Conda executable not found.")
+
+    # Get the current Conda configuration for channels
+    result = subprocess.run([conda_PATH, "config", "--show", "channels"], capture_output=True, text=True)
+    channels = result.stdout
+
+    # Check if 'conda-forge' is in the channels list
+    if 'conda-forge' not in channels:
+        # If 'conda-forge' is not in the channels, add it
+        subprocess.run([conda_PATH, "config", "--add", "channels", "conda-forge"])
+        print("Added conda-forge to channels.")
+    
+    for package in dependencies:
+        print(f"Installing {package}")
+        subprocess.run([conda_PATH, "install", "-n", env_name, package, "-y"])
+    
+    # Install additional packages
+    subprocess.run([pip_PATH, "install", "opencv-python-headless"])
+    subprocess.run([pip_PATH, "install", "PyQt5"])
+    subprocess.run([pip_PATH, "install", "screeninfo"])
+
+    print("Dependencies installation complete.")
+
+env_name = "spacr_modify_masks_tkinter"
+
+conda_PATH, python_PATH, pip_PATH, env_PATH = get_paths(env_name)
+
+dependencies = ["tkinter", "imageio", "scipy", "pillow", "scikit-image", "ipykernel", "requests", "h5py", "pyzmq"]
+
+if not os.path.exists(env_PATH):
+
+    print(f'System type: {sys.platform}')
+    print(f'PATH to conda: {conda_PATH}')
+    print(f'PATH to python: {python_PATH}')
+    print(f'PATH to pip: {pip_PATH}')
+    print(f'PATH to new environment: {env_PATH}')
+
+    create_environment(conda_PATH, env_name)
+    install_dependencies_in_kernel(dependencies, env_name)
+    add_kernel(env_name, env_name)
+    print(f"Environment '{env_name}' created and added as a Jupyter kernel.")
+    print(f"Refresh the page, set {env_name} as the kernel and run cell again")
+    sys.exit()
+    #SystemExit()
+
+#######################################################################################################################
+
 import os
 import numpy as np
 import tkinter as tk
@@ -9,7 +124,6 @@ from skimage.draw import polygon
 from skimage.transform import resize
 from skimage.measure import label as sk_label
 from scipy.ndimage import binary_fill_holes, label
-
 
 class modify_masks:
     def __init__(self, root, folder_path, scale_factor, width, height):
